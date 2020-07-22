651f7e7fb6341034772abdef78efba59beacdebf
olivia <wangol@oocl.com>
2020-07-23 00:54:23 +0800

    oo[olivia]feature:practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,36 @@
 package practice08;
 
 public class Klass {
+    public int number;
+
+    public Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setStudent(Student leader) {
+        this.leader = leader;
+    }
+
+    public String getDisplayName() {
+        return "Class "+this.number;
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
