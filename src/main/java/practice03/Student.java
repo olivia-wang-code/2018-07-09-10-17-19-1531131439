123bbdb8324a01e9686e84a5a625f8e755021feb
olivia-wang-code <2350625418@qq.com>
2020-07-22 17:14:30 +0800

    oo[olivia]feature:practice03 and test.java for three practices

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,29 @@
 package practice03;
 
 public class Student {
+    private String name;
+    private int age;
+    private int classNUmber;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return classNUmber;
+    }
+
+    public Student(String name, int age, int classNUmber){
+        this.name=name;
+        this.age=age;
+        this.classNUmber=classNUmber;
+    }
+    public String introduce(){
+        return "I am a Student. I am at Class 2.";
+    }
+    
 }
