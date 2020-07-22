e6e46ff176848416d343d6a57f5080141cbceac7
olivia-wang-code <2350625418@qq.com>
2020-07-22 17:02:13 +0800

    oo[olivia]feature:practice01 and practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,29 @@
 package practice02;
 
 public class Student {
+    private String name;
+    private int age;
+    private int classNUmber;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return classNUmber;
+    }
+
+    public Student(String name, int age, int classNUmber){
+        this.name=name;
+        this.age=age;
+        this.classNUmber=classNUmber;
+    }
+    public String introduce(){
+        return "My name is Tom. I am 21 years old.";
+    }
+    
 }
