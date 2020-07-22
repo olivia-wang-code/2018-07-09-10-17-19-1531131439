904b70e9a159a2eb1da243f38e37cd9c5c21251d
olivia-wang-code <2350625418@qq.com>
2020-07-22 19:01:57 +0800

    oo[olivia]feature:practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,22 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+    public Teacher(String name,int age,Integer klass){
+        super(name,age);
+        this.klass=klass;
+    }
+    public Teacher(String name,int age){
+        super(name,age);
+        this.klass=klass;
+    }
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        if(klass==null){
+            return super.introduce()+" I am a Teacher. I teacher No Class.";
+        }
+        return super.introduce()+" I am a teacher. I teacher Class "+this.klass+".";
+    }
 }
