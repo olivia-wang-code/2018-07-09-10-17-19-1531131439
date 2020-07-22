7f542c175bee7b5cd3a820c9d427b3870c7a9174
olivia-wang-code <2350625418@qq.com>
2020-07-22 18:28:36 +0800

    oo[olivia]feature:practice04

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,16 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+    private int classNUmber;
+    public Student(String name,int age,int classNUmber){
+        super(name,age);
+        this.classNUmber=classNUmber;
+    }
+    
+    public int getKlass() {
+        return classNUmber;
+    }
+    public String introduce(){
+        return super.introduce()+" I am a Student. I am at Class "+this.classNUmber+".";
+    }
 }
