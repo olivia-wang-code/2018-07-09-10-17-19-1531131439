a87990df57cae0ede943f60cfbaa2ded6890963f
olivia-wang-code <2350625418@qq.com>
2020-07-22 19:49:42 +0800

    oo[olivia]feature:practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,14 @@
 package practice07;
 
 public class Klass {
+    private int num;
+    public Klass(int num){
+        this.num=num;
+    }
+    public int getNumber(){
+        return this.num;
+    }
+    public String getDisplayName(){
+        return "Class "+this.num;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,30 @@
 package practice07;
 
-public class Teacher {
+import practice06.Person;
+
+public class Teacher extends Person {
+    private Klass klass;
+    public Teacher(String name,int age,Klass klass){
+        super(name,age);
+        this.klass=klass;
+    }
+    public Teacher(String name,int age){
+        super(name,age);
+        this.klass=klass;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        if(klass==null){
+            return super.introduce()+" I am a Teacher. I teacher No Class.";
+        }
+        return super.introduce()+" I am a teacher. I teacher Class ."+this.klass;
+    }
+    public String introduceWith(Student student){
+        if(student.getKlass().getNumber()==this.klass.getNumber()){
+            return super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }
+        return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+    }
 }
